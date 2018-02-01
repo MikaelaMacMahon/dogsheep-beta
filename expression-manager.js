55c12ba6a070121682826682a7cab300a06f3cb8
Mikaela MacMahon <mikaela.macmahon@gmail.com>
2018-02-01 13:45:28 -0500

    Debugging input

--- expression-manager.js
+++ expression-manager.js
@@ -3,16 +3,22 @@
 //event listeners
 var btn = document.getElementById("submitBtn");
 //error handling for different browsers
+btn.addEventListener("click", processInput);
 if(btn.addEventListener){
-    btn.addEvenListener("click", processInput);
+    btn.addEventListener("click", processInput);
 } else if(btn.attachEvent){
     btn.attachEvent("onclick", processInput);
 }
 
+
+//TODO:: add enter listener
+
 function processInput(){
     //retrieve input text
-    var inputEx = 5; //FIX
+    var textbox = document.getElementById("expr");
+    var inputEx = texbox.value;
     //check validity of expression
+    textbox.value = "WORKS";
     if(!checkValidity(inputEx)){
         //return error
         //TODO:: Return notification to user about incorrect format
@@ -20,9 +26,11 @@ function processInput(){
      }
      
 
-     //maybe set up a class or some way of tagging an expression (object with type and expression
-     //process expressions
+    //maybe set up a class or some way of tagging an expression (object with type and expression
+    //process expressions
 
+    //clear input textbook
+    textbox.value = "";
 
 }
 
