9e77b5477d57c80d300e932ea8e70ccaf06f8d4b
Mikaela MacMahon <mikaela.macmahon@gmail.com>
2018-02-25 19:45:37 -0500

    Got get working

--- simplify-server.js
+++ simplify-server.js
@@ -9,6 +9,7 @@ var app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
     next();
 });
 //TODO:: add get for first page
